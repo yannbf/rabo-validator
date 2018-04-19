68048ba4bf032d60b83d87897d4d021e358364fc
Yann Braga <yannbf@gmail.com>
2018-04-19 23:44:01 +0200

    feat(file-input): add input file on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,4 +8,19 @@ import { Ng2FileInputService, Ng2FileInputAction } from 'ng2-file-input';
 })
 export class HomeComponent {
   constructor() {}
+
+  public onAction(event: any) {
+    console.log(event);
+    console.log(this.getFileNames(event.currentFiles));
+  }
+
+  public onAdded(event: any) {
+    console.log('FileInput: ' + event.id);
+    console.log('Action: File added');
+  }
+
+  private getFileNames(files: File[]): string {
+    const names = files.map(file => file.name);
+    return names ? names.join(', ') : 'No files currently added.';
+  }
 }
