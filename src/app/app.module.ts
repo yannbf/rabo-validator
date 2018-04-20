5e41d1897d7454c277fc4ca5fe4fa516ef2d44c6
Yann Braga <yannbf@gmail.com>
2018-04-20 10:30:18 +0200

    feat(package): add papaparse to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { Ng2FileInputModule } from 'ng2-file-input';
+import { PapaParseModule } from 'ngx-papaparse';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages';
@@ -9,7 +10,12 @@ import { FormatConverterService } from './services/format-converter.service';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, Ng2FileInputModule.forRoot()],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    Ng2FileInputModule.forRoot(),
+    PapaParseModule,
+  ],
   providers: [FormatConverterService],
   bootstrap: [AppComponent],
 })
