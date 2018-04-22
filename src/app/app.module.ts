b78b67212b661c5a9d4a1f37a898d3f4828caab9
Yann Braga <yannbf@gmail.com>
2018-04-22 13:48:37 +0200

    feat(lib): add ngx-toastr to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Ng2FileInputModule } from 'ng2-file-input';
 import { PapaParseModule } from 'ngx-papaparse';
+import { DataTableModule } from 'angular2-datatable';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages';
 import { FormatConverterService } from './services/format-converter.service';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
-import { DataTableModule } from 'angular2-datatable';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, TransactionsTableComponent],
@@ -18,6 +20,8 @@ import { DataTableModule } from 'angular2-datatable';
     Ng2FileInputModule.forRoot(),
     PapaParseModule,
     DataTableModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [FormatConverterService],
   bootstrap: [AppComponent],
