e0df7ad9e9585b81929de91fc5ef497e46a97315
Yann Braga <yannbf@gmail.com>
2018-04-23 13:07:11 +0200

    feat(util): add XML validation function

--- src/app/shared/util/index.ts
+++ src/app/shared/util/index.ts
@@ -8,10 +8,17 @@ export const checkTransactionProperties = (obj: any): boolean =>
   obj.hasOwnProperty('startBalance') &&
   obj.hasOwnProperty('reference');
 
-export const hasDuplicates = (array: Array<any>, value: any) =>
+export const hasDuplicates = (array: Array<any>, value: any): boolean =>
   array.filter(v => v === value).length > 1;
 
-export const camelize = (str: string) =>
+export const camelize = (str: string): string =>
   str
     .split('')
     .reduce((t, v, k) => t.trim() + (k === 0 ? v.toLowerCase() : v), '');
+
+export const validateXML = (text: string): boolean => {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(text, 'text/xml');
+
+  return xmlDoc.getElementsByTagName('parsererror').length <= 0;
+};
