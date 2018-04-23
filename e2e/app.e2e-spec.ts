a49c33cfe5c0a7329c8b71f948360732f92d9d55
Yann Braga <yannbf@gmail.com>
2018-04-23 15:54:34 +0200

    chore(app): general cleaning

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('real-estatium App', () => {
+describe('Rabo Validator App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display proper title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getAppTitle()).toEqual('Rabo validator');
   });
 });
